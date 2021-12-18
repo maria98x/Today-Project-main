2ea8a93dab46110c5ccd909fd98e5d7e042dc074
maria98x <53050052+maria98x@users.noreply.github.com>
2021-12-19 01:40:30 +0300

    v3

--- api.js
+++ api.js
@@ -1,42 +1,50 @@
-const url = "http://api.weatherbit.io/v2.0/current?city=Riyadh&lat=35.7796&lon=-78.6382&key=d212eb8e85624b3085b4f6c368bc209c&include=minutely";
 
 
-fetch(url).then((res)=> {
-    res.json().then((res)=>{
-        console.log(res.data[0]);
-        let time = res.data[0].ob_time;
-        let subtime = time.substr(time.length - 5);
-        let d = `
-        <div class="d-flex">
-        <h3 class="flex-grow-1 " id ="cityName">${res.data[0].city_name}</h3>
-        <h6 id="time">${subtime}</h6>
-      </div>
-    
-      <div class="d-flex flex-column text-center mt-5 mb-4">
-        <h6 class="display-4 mb-0 font-weight-bold" style="color: #1C2331;" id="deg"> ${res.data[0].temp}°C </h6> 
-        <span class="small" style="color: #868B94" id="stat">${res.data[0].weather.description}</span>
-      </div>
-    
-      <div class="d-flex align-items-center">
-        <div class="flex-grow-1" style="font-size: 1rem;">
-          <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].wind_spd} km/h </span></div> 
-          <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].rh}% </span></div> 
-          <div><i class="fas fa-sun fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 0.2h </span></div>
-        </div>
-        <div>
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.png" width="100px">
-        </div>
-      </div>
-    
+const searchInput = document.querySelector("#search-input");
+const searchBut = document.querySelector("#search-addon");
+
+let    url = 'https://api.weatherbit.io/v2.0/current?city=Jeddah&key=d212eb8e85624b3085b4f6c368bc209c&include=minutely';
+setWeather ()
+searchBut.addEventListener("click",function(){
+    url = 'https://api.weatherbit.io/v2.0/current?city='+searchInput.value+'&key=d212eb8e85624b3085b4f6c368bc209c&include=minutely';
+    setWeather ()
+})
+
+function setWeather (){
+    fetch(url).then((res)=> {
+        res.json().then((res)=>{          
+            let time = res.data[0].ob_time;
+            let subtime = time.substr(time.length - 5);
+            let d = `
+            <div class="d-flex">
+            <h3 class="flex-grow-1 text-black" id ="cityName">${res.data[0].city_name}</h3>
+            <h6 class="text-black"id="time">${subtime}</h6>
+          </div>
         
-        `;
-
-        document.getElementById("weatherData").innerHTML = d;
-
+          <div class="d-flex flex-column text-center mt-5 mb-4">
+            <h6 class="display-4 mb-0 text-black"  id="deg"> <strong> ${res.data[0].temp}°C </strong></h6> 
+            <span class="small text-black" style="color: #868B94" id="stat">${res.data[0].weather.description}</span>
+          </div>
+        
+          <div class="d-flex align-items-center text-black">
+            <div class="flex-grow-1 text-black" style="font-size: 1rem;">
+              <div><i class="fas fa-wind fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].wind_spd} km/h </span></div> 
+              <div><i class="fas fa-tint fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].rh}% </span></div> 
+              <div><i class="fas fa-sun fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1 "> 0.2h </span></div>
+            </div>
+            <div>
+             <!-- pic-->
+            </div>
+          </div>
+            
+            `;
+    
+            document.getElementById("weatherData").innerHTML = d;
+    
+        });
+    
+      
+    
     });
-
-  
-
-});
-
+}
 
