813238b6519606e2f3441288d4e51197bd5c781d
maria98x <53050052+maria98x@users.noreply.github.com>
2021-12-19 23:52:10 +0300

    v4

--- api.js
+++ api.js
@@ -1,50 +1,108 @@
 
-
 const searchInput = document.querySelector("#search-input");
 const searchBut = document.querySelector("#search-addon");
 
+const searchInput2 = document.querySelector("#searchInput2");
+const searchBut2 = document.querySelector("#searchBut2");
+
 let    url = 'https://api.weatherbit.io/v2.0/current?city=Jeddah&key=d212eb8e85624b3085b4f6c368bc209c&include=minutely';
+let url2 = 'https://newsapi.org/v2/top-headlines?category=General&sortBy=popularity&apiKey=5ba5a686789b445bbe3ebae32358f4bb'
+
+setNews();
+$('ul.nav-tabs > li').click(function (e) {
+  console.log("heeeerereeeeeeeee")
+  e.preventDefault();
+  var getItem = $(this).text();
+  var id = this.id;
+  url2 = 'https://newsapi.org/v2/top-headlines?category='+ id +'&from=2021-12-19&sortBy=popularity&apiKey=5ba5a686789b445bbe3ebae32358f4bb';
+  setNews();
+
+});
+
+searchBut2.addEventListener("click",function(){
+  url2 = 'https://newsapi.org/v2/everything?q='+searchInput2.value+'&sortBy=popularity&apiKey=5ba5a686789b445bbe3ebae32358f4bb';
+  setNews();
+})
+
+
 setWeather ()
 searchBut.addEventListener("click",function(){
     url = 'https://api.weatherbit.io/v2.0/current?city='+searchInput.value+'&key=d212eb8e85624b3085b4f6c368bc209c&include=minutely';
     setWeather ()
 })
 
+
 function setWeather (){
-    fetch(url).then((res)=> {
-        res.json().then((res)=>{          
-            let time = res.data[0].ob_time;
-            let subtime = time.substr(time.length - 5);
-            let d = `
-            <div class="d-flex">
-            <h3 class="flex-grow-1 text-black" id ="cityName">${res.data[0].city_name}</h3>
-            <h6 class="text-black"id="time">${subtime}</h6>
-          </div>
-        
-          <div class="d-flex flex-column text-center mt-5 mb-4">
-            <h6 class="display-4 mb-0 text-black"  id="deg"> <strong> ${res.data[0].temp}°C </strong></h6> 
-            <span class="small text-black" style="color: #868B94" id="stat">${res.data[0].weather.description}</span>
-          </div>
-        
-          <div class="d-flex align-items-center text-black">
-            <div class="flex-grow-1 text-black" style="font-size: 1rem;">
-              <div><i class="fas fa-wind fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].wind_spd} km/h </span></div> 
-              <div><i class="fas fa-tint fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].rh}% </span></div> 
-              <div><i class="fas fa-sun fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1 "> 0.2h </span></div>
-            </div>
-            <div>
-             <!-- pic-->
-            </div>
-          </div>
-            
-            `;
-    
-            document.getElementById("weatherData").innerHTML = d;
-    
-        });
-    
+  fetch(url).then((res)=> {
+      res.json().then((res)=>{          
+          let time = res.data[0].ob_time;
+          let subtime = time.substr(time.length - 5);
+          let d = `
+          <div class="d-flex">
+          <h3 class="flex-grow-1 text-black" id ="cityName">${res.data[0].city_name}</h3>
+          <h6 class="text-black"id="time">${subtime}</h6>
+        </div>
       
+        <div class="d-flex flex-column text-center mt-5 mb-4">
+          <h6 class="display-4 mb-0 text-black"  id="deg"> <strong> ${res.data[0].temp}°C </strong></h6> 
+          <span class="small text-black" style="color: #868B94" id="stat">${res.data[0].weather.description}</span>
+        </div>
+      
+        <div class="d-flex align-items-center text-black">
+          <div class="flex-grow-1 text-black" style="font-size: 1rem;">
+            <div><i class="fas fa-wind fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].wind_spd} km/h </span></div> 
+            <div><i class="fas fa-tint fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1"> ${res.data[0].rh}% </span></div> 
+            <div><i class="fas fa-sun fa-fw text-black" style="color: #868B94;"></i> <span class="ms-1 "> 0.2h </span></div>
+          </div>
+          <div>
+           <!-- pic-->
+          </div>
+        </div>
+          
+          `;
+  
+          document.getElementById("weatherData").innerHTML = d;
+  
+      });
+  
     
+  
+  });
+}
+
+function setNews(){
+  fetch(url2).then((res) => {
+    res.json().then((res)=>{
+    document.getElementById("newsData").innerHTML = res.articles.map(ar =>
+        
+         `  <div class="col-md-6 col-lg-4 mx-auto my-2" >
+    
+         <div class="card shadow cardSize">
+         <img
+           src="${ar.urlToImage}"
+           class="card-img-top t"
+           alt="Hollywood Sign on The Hill"
+         />
+         <div class="card-body ">
+         <p class="card-text ">${ar.publishedAt.split("T")[0]} </p>
+           <h5 class="card-title ">${ar.title}</h5>
+           <p class="card-text">
+            ${ar.description}
+           </p>
+           <p class="card-text">${ar.author} </p>
+           
+          
+         </div>
+         <div class="card-footer">  <a href="${ar.url}" class="btn myColor text-white ">Read more </a></div>
+    
+       </div>
+         </div>
+    `
+    
+        )  .join("");
     });
+    });
+    
+    
 }
 
